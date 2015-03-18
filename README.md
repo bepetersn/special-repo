6b83010b3508d1cd8f7eaef60f5c2b15dd3c62ca
Brian Peterson <bepetersn@gmail.com>
2015-03-18 09:42:28 -0500

    Comments and instructions

---
+++ README.md
@@ -0,0 +1,5 @@
+To see this bug, run `python server.py` in one process.
+Then, open a new terminal and run `python client.py`. 
+
+See the print statements and comments in server.py for 
+my observations.
--- server.py
+++ server.py
@@ -6,8 +6,14 @@ app = Flask(__name__)
 
 @app.route("/api/request-coffee", methods=['POST'])
 def hello():
-    print request.form.keys()
-    print request.files.keys()
+
+    # Oddly, `request.form` has a `None` key that has the
+    # contents of the file that was posted, while
+    # `request.files` is empty.
+
+    print('request.form: {}'.format(request.form.keys()))
+    print('request.files: {}'.format(request.files.keys()))
+    import pdb; pdb.set_trace()
 
 
 if __name__ == "__main__":
